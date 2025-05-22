dd503537b6510ebb0f5a6dde7932643553402565
Md Julhas <139030290+md-julhas@users.noreply.github.com>
2025-05-22 19:42:33 +0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://julhas120:julhasxmz.5323.49@cluster0.a1ywe.mongodb.net/restaurant"
-    )
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("Connection to DB is successfully established")
   } catch (error) {
-    console.log("Error connecting to DB:", error.toString())
+    console.error("Error connecting to DB:", error.message)
   }
 }

e0c1bc2fee4522f03fe49726ae1891f98129d423
Md Julhas <139030290+md-julhas@users.noreply.github.com>
2025-05-24 11:46:19 +0600

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -25,7 +25,7 @@ const upload = multer({ storage: storage })
 // Strict limiter for user creation (very low limit)
 const createUserLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 1,
+  max: 5,
   message:
     "Too many accounts created from this IP, please try again after 5 minutes",
 })
